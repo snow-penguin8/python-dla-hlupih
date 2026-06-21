6e7b71d1bfcceff01a76dbc94da8051f14a15c27
snow-penguin8 <penguinsneg13@gmail.com>
2026-06-21 15:10:25 +0200

    1

---
+++ main.py
@@ -0,0 +1,198 @@
+from random import randint,choice
+randnum = randint(0, 2)
+
+
+students = ["cat ", "ivan", "leha"]
+
+
+print(f"randchose:{choice(students)}")
+print(f"randnum:{students[randint(0, 2)]}")
+
+
+
+#
+#
+# students = [3, 2, 4]
+#
+# students.sort(reverse=True)
+#
+# print(students)
+#
+
+
+
+#
+#
+# list_a = [1,2,3]
+#
+# list_b = list_a
+#
+#
+#
+#
+# list_b.append(8)
+#
+# print(list_a) ВЫВОДИТ ТОТ ЖЕ
+#
+#
+
+
+
+
+
+
+
+# x = True
+# while(x):
+#     try:
+#         print ("1 число")
+#         a = int(input())
+#         if (isinstance(a, int)):
+#             break
+#         else:
+#             continue
+#     except ValueError:
+#         print("ТВАРЬ ТУТ ЦИФРИ НАДО")
+#         x = True
+#
+# x = True
+# while(x):
+#
+#     try:
+#         print ("2 число")
+#         b = int(input())
+#         if (isinstance(b, int)):
+#             break
+#         else:
+#             continue
+#     except ValueError:
+#         print("ТВАРЬ ТУТ ЦИФРИ НАДО")
+#         x = True
+#
+# x = True
+# while(x):
+#     try:
+#         print ("operation")
+#         c = str(input())
+#         if (isinstance(c, str)):
+#             break
+#         else:
+#            continue
+#     except ValueError:
+#         print("ТВАРЬ ТУТ OPERATII НАДО")
+#
+# match c:
+#     case "+":
+#         print(a + b)
+#     case '-':
+#         print(a - b)
+#     case '**':
+#         print(a ** b)
+#     case '*':
+#         print(a * b)
+#     case '/':
+#         print(a / b)
+#     case _:
+#         print("no")
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def print_A():
+#     y = 6
+#     print(y)
+#     pass
+# print_A()
+# print(y) ОН ТУТ НЕ СУЩЕСТВУЕТ
+
+
+
+
+
+
+# students = ["cat ", "ivan", "leha"]
+# sus = ["umo"]
+#
+# # print(students)
+# students.append("koma")
+# # print(students)
+# students.remove(students[3])
+# print(students)
+#
+# s = "IVAN"
+#
+#
+#
+#
+#
+# for i in range(4):
+#     print(f"{i}:{s[i]} ")
+#
+#
+# player = {
+#     "name":"IVAN",
+#     "age": 17,
+#     "city":"leha"
+# }
+
+#
+# player["vector"] = 30
+#
+#
+# player["city"] = "CHELABINSK"
+#
+#
+# print(player["vector"])
+# print(player)
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
